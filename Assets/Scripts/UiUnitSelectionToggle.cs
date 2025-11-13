a601a2599475a856a9a61ab2f2038afccb2fb225
AraB-Blue <abediser@gmail.com>
2025-11-13 13:51:36 +0100

    code

---
+++ Assets/Scripts/UiUnitSelectionToggle.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class UiUnitSelectionToggle : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] elementUIToToggle;
+
+
+    void Update()
+    {
+        string unitSelection = UnitSelection.Instance.selectedUnit != null
+            ? UnitSelection.Instance.selectedUnit.name
+            : null;
+
+        switch (unitSelection)
+        {
+            case null:
+                for (int i = 0; i < elementUIToToggle.Length; i++)
+                {
+                    elementUIToToggle[i].SetActive(false);
+                }
+                break;
+
+            case "Chomper":
+                DeactivateAllUIElements();
+                if (elementUIToToggle.Length > 0)
+                    elementUIToToggle[0].SetActive(true);
+                break;
+
+            case "Ellen":
+                DeactivateAllUIElements();
+                if (elementUIToToggle.Length > 1)
+                    elementUIToToggle[1].SetActive(true);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void DeactivateAllUIElements()
+    {
+        for (int i = 0; i < elementUIToToggle.Length; i++)
+        {
+            elementUIToToggle[i].SetActive(false);
+        }
+    }
+}
+
