46764ff6ec86056840528203e66392436373de3b
AraB-Blue <abediser@gmail.com>
2025-11-27 12:14:59 +0100

    scripts_unit

--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class EnemyCharacter : Character
 {
+    [SerializeField] Weapon equippedWeapon;
+    [SerializeField] Equipment equippedEquipment;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
