e0e41cceb3f5f29234910ce56e15778096d741bd
AraB-Blue <abediser@gmail.com>
2025-12-05 13:22:59 +0100

    script_playerCharacter

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,102 +1,70 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.TextCore.Text;
 
 public class PlayerCharacter : Character
 {
     float experience;
-    Weapon equippedWeapon;
-    Equipment equippedEquipment;
+
+    [SerializeField] Weapon equippedWeapon;
+
+    [SerializeField] Equipment equippedEquipment;
+
     [SerializeField] List<Equipment> equipmentList = new List<Equipment>();
+
+
     [SerializeField] List<Weapon> weaponList = new List<Weapon>();
 
     public GameObject targetSelectionPanel;
-    public GameObject weaponSelectionPanel;
 
 
     void Start()
     {
-        if (weaponList.Count > 0)
-            equippedWeapon = weaponList[0];
+        
+        if (equippedWeapon != null)
+            Debug.Log(gameObject.name + " usa el arma fija: " + equippedWeapon.GetWeaponName());
 
-        if (equipmentList.Count > 0)
+        
+        if (equippedEquipment == null && equipmentList.Count > 0)
             equippedEquipment = equipmentList[0];
 
+        
         if (targetSelectionPanel != null)
             targetSelectionPanel.SetActive(false);
-
-        if (weaponSelectionPanel != null)
-            weaponSelectionPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void Earnexperience(float expGain)
-    {
-        experience += expGain;
-    }
-
-    void LevelUp()
-    {
-        level++;
-    }
 
     public Weapon GetEquippedWeapon()
     {
         return equippedWeapon;
     }
 
+
+    
     public void EquipWeapon(int weaponIndex)
     {
-        if (weaponIndex >= 0 && weaponIndex < weaponList.Count)
-        {
-            equippedWeapon = weaponList[weaponIndex];
-            Debug.Log(gameObject.name + " equipó " + equippedWeapon.GetWeaponName());
-
-            if (weaponSelectionPanel != null)
-                weaponSelectionPanel.SetActive(false);
-        }
+        Debug.LogWarning(gameObject.name + " no puede cambiar de arma.");
+        return;
     }
 
+
+    
     public List<Weapon> GetWeaponList()
     {
-        return weaponList;
+        return new List<Weapon>();
     }
 
-    public void ShowWeaponSelectionPanel()
-    {
-        if (weaponSelectionPanel != null)
-            weaponSelectionPanel.SetActive(true);
-    }
-
-    public void HideWeaponSelectionPanel()
-    {
-        if (weaponSelectionPanel != null)
-            weaponSelectionPanel.SetActive(false);
-    }
 
     public override float GetTotalArmor()
     {
-        // Asegurarse de que el equipamiento esté inicializado
         if (equippedEquipment == null && equipmentList.Count > 0)
-        {
             equippedEquipment = equipmentList[0];
-        }
 
         float totalArmor = 0f;
 
-        // Sumar armadura de todo el equipamiento equipado
         if (equippedEquipment != null)
-        {
             totalArmor = equippedEquipment.GetArmor();
-            Debug.Log(gameObject.name + " - Armadura del equipamiento " + equippedEquipment.name + ": " + totalArmor);
-        }
-
 
         return totalArmor;
     }
 }
+
